d5daedb912c1f48e9d964e50919fb7fd451e9ff3
Ömer Faruk Görmel <gormelomerfaruk@gmail.com>
2018-02-24 21:23:44 +0300

    kayıt ol ve şifremi unuttum ekranları tasarlandı, başlangıç sayfasındaki ekran geçişleri tamamlandı

--- app/src/main/java/gormelof/net/sausozluk/views/entrypoint/LoginFragment.java
+++ app/src/main/java/gormelof/net/sausozluk/views/entrypoint/LoginFragment.java
@@ -1,15 +1,14 @@
 package gormelof.net.sausozluk.views.entrypoint;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import gormelof.net.sausozluk.R;
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginFragment extends Fragment {
 
@@ -28,10 +27,31 @@ public class LoginFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View loginView = inflater.inflate(R.layout.fragment_login, container, false);
+
+        TextView tvGoRegister = loginView.findViewById(R.id.tv_fragment_login_go_register_page);
+        TextView tvGoForgetPassword = loginView.findViewById(R.id.tv_fragment_login_go_forgot_password_page);
+
+        tvGoRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fl_activity_entry_point_fragment_container, RegisterFragment.newInstance());
+                transaction.commit();
+            }
+        });
+
+        tvGoForgetPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fl_activity_entry_point_fragment_container, ForgotPasswordFragment.newInstance());
+                transaction.commit();
+            }
+        });
+
+        return loginView;
     }
 
 }
