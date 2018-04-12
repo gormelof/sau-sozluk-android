1a1577d09f50762b452c9d7eebef254b949221e9
Ömer Faruk Görmel <gormelomerfaruk@gmail.com>
2018-04-12 21:31:20 +0300

    profil ekranı tasarlandı, ekrandaki view'lar bind edildi

--- app/src/main/java/gormelof/net/sausozluk/views/ui/main/ProfileFragment.java
+++ app/src/main/java/gormelof/net/sausozluk/views/ui/main/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import gormelof.net.sausozluk.R;
 
@@ -26,9 +27,47 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        final View profileView = inflater.inflate(R.layout.fragment_profile, container, false);
 
-        return view;
+        TextView tvProfileUsername = profileView.findViewById(R.id.tv_fragment_profile_username);
+        TextView tvProfileStatus = profileView.findViewById(R.id.tv_fragment_profile_status);
+        TextView tvProfileGeneration = profileView.findViewById(R.id.tv_fragment_profile_generation);
+        TextView tvEntryCount = profileView.findViewById(R.id.tv_fragment_profile_entry_count);
+
+        TextView tvProfileChangePassword = profileView.findViewById(R.id.tv_fragment_profile_change_password);
+        TextView tvProfileChangeEmail = profileView.findViewById(R.id.tv_fragment_profile_change_email);
+        TextView tvProfileDestroySession = profileView.findViewById(R.id.tv_fragment_profile_destroy_session);
+        TextView tvProfileDestroyAllSession = profileView.findViewById(R.id.tv_fragment_profile_destroy_all_sessions);
+
+        tvProfileChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: go change password fragment
+            }
+        });
+
+        tvProfileChangeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: go change email fragment
+            }
+        });
+
+        tvProfileDestroySession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: destroy session
+            }
+        });
+
+        tvProfileDestroyAllSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // todo: destroy all sessions
+            }
+        });
+
+        return profileView;
     }
 
 }
